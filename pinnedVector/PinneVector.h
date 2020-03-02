a19bbe197da9312882efb0b5f840b13b8da8bbf3
vlod <vladalex420@gmail.com>
2020-03-02 22:28:38 +0200

    made it work

--- pinnedVector/PinneVector.h
+++ pinnedVector/PinneVector.h
@@ -1,29 +1,70 @@
 #pragma once
 #include <Windows.h>
+#include <assert.h>
 
-template <class T>
+#ifdef _DEBUG
+
+#define PINNED_VECTOR_DEBUG(x) x
+#define PINNED_VECTOR_ASSERT(x) assert(x)
+
+#else
+
+#define PINNED_VECTOR_DEBUG(x) 
+#define PINNED_VECTOR_ASSERT(x) 
+
+#endif // DEBUG
+
+
+
+template <class T, unsigned int maxElCount = 12000>
 class PinneVector
 {
+public:
+
+	PinneVector()
+	{
+		initializeArena();
+	}
+
 	void *begin = 0;
 	unsigned int size = 0;
-	size_t actualSizeInBytes = 0;
+	//size_t actualSizeInBytes = 0;
 
 
 	void initializeArena();
 
 	void resize(unsigned int elementCount);
 
+	T &operator[] (unsigned int index)
+	{
+		PINNED_VECTOR_ASSERT(index < size);
+		return static_cast<T*>(begin)[index];
+	}
 
+	T operator[] (unsigned int index) const
+	{
+		PINNED_VECTOR_ASSERT(index < size);
+		return static_cast<T*>(begin)[index];
+	}
 };
 
-template<class T>
-inline void PinneVector<T>::initializeArena()
+template<class T, unsigned int maxElCount>
+inline void PinneVector<T, maxElCount>::initializeArena()
 {
-
+	
+	begin = VirtualAlloc(0, sizeof(T) * maxElCount, MEM_RESERVE, PAGE_READWRITE);
+	PINNED_VECTOR_ASSERT(begin != nullptr);
 
 }
 
-template<class T>
-inline void PinneVector<T>::resize(unsigned int elementCount)
+template<class T, unsigned int maxElCount>
+inline void PinneVector<T, maxElCount>::resize(unsigned int elementCount)
 {
+	if(elementCount > size)
+	{
+		VirtualAlloc(begin, elementCount * sizeof(T), MEM_COMMIT, PAGE_READWRITE);
+		size = elementCount;
+	}
+
+
 }
--- pinnedVector/main.cpp
+++ pinnedVector/main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
-
-
-
+#include "PinneVector.h"
 
 int main()
 {
-	
+	PinneVector<int> v;
+
+	v.resize(10);
+
+	v[0] = 1;
+
+	std::cout << v[0] << "\n";
+
+	v.resize(1200);
+
+	std::cout << v[0] << "\n";
 
 	return 0;
 }
