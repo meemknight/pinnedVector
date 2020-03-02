b44c9f5568f294c229f9afbccb4d7dbb666fc23d
vlod <vladalex420@gmail.com>
2020-03-02 23:37:21 +0200

    PinnedVector &operator= (const PinnedVector& other)

--- pinnedVector/PinneVector.h
+++ pinnedVector/PinneVector.h
@@ -2,6 +2,14 @@
 #include <Windows.h>
 #include <assert.h>
 
+// /////////////////
+// Pinned vector
+// Define PINNED_VECTOR_MEMORY_CHECK to check if acces deleted memory areas
+// 
+//
+
+//#define PINNED_VECTOR_MEMORY_CHECK
+
 #ifdef _DEBUG
 
 #define PINNED_VECTOR_DEBUG(x) x
@@ -16,24 +24,58 @@
 
 
 
-template <class T, unsigned int maxElCount = 12000>
-class PinneVector
-{
-public:
 
-	PinneVector()
+template <class T, unsigned int maxElCount = 12000>
+struct PinnedVector
+{
+	void *begin = 0;
+	unsigned int size = 0;
+
+	PinnedVector()
 	{
 		initializeArena();
 	}
 
-	void *begin = 0;
-	unsigned int size = 0;
-	//size_t actualSizeInBytes = 0;
+	PinnedVector(PinnedVector &&other)
+	{
+		this->begin = other.begin;
+		this->size = other.size;
 
+		other.size = 0;
+		other.begin = nullptr;
+	
+		//?
+		other.initializeArena();
+	}
+
+	PinnedVector(PinnedVector &other)
+	{
+		initializeArena();
+		resize(other.size);
+
+		memcpy(begin, other.begin, size * sizeof(T));
+	}
+
+
+	static constexpr unsigned int maxSize = maxElCount;
 
 	void initializeArena();
 
 	void resize(unsigned int elementCount);
+
+	PinnedVector &operator= (const PinnedVector& other)
+	{
+		this->resize(other.size);
+		memcpy(begin, other.begin, size * sizeof(T));
+		return *this;
+	}
+
+	PinnedVector &operator= (const PinnedVector&& other)
+	{
+		free();
+		this(std::move(other));
+		return *this;
+	}
 
 	T &operator[] (unsigned int index)
 	{
@@ -46,25 +88,76 @@ public:
 		PINNED_VECTOR_ASSERT(index < size);
 		return static_cast<T*>(begin)[index];
 	}
+
+	void push_back(const T& el);
+	void pop_back();
+
+	void clear();
+
+	void free();
+
+	~PinnedVector();
 };
 
 template<class T, unsigned int maxElCount>
-inline void PinneVector<T, maxElCount>::initializeArena()
+inline void PinnedVector<T, maxElCount>::initializeArena()
 {
-	
+	if(begin)
+	{
+		free();
+	}
+
 	begin = VirtualAlloc(0, sizeof(T) * maxElCount, MEM_RESERVE, PAGE_READWRITE);
 	PINNED_VECTOR_ASSERT(begin != nullptr);
 
 }
 
 template<class T, unsigned int maxElCount>
-inline void PinneVector<T, maxElCount>::resize(unsigned int elementCount)
+inline void PinnedVector<T, maxElCount>::resize(unsigned int elementCount)
 {
+	if(!begin)
+	{
+		initializeArena();
+	}
+
 	if(elementCount > size)
 	{
 		VirtualAlloc(begin, elementCount * sizeof(T), MEM_COMMIT, PAGE_READWRITE);
 		size = elementCount;
+	}else 
+	{
+		elementCount = size;
 	}
 
+}
 
+template<class T, unsigned int maxElCount>
+inline void PinnedVector<T, maxElCount>::free()
+{
+
+#ifdef PINNED_VECTOR_MEMORY_CHECK
+
+	if (begin)
+	{
+		//VirtualFree(begin, maxElCount * sizeof(T), MEM_DECOMMIT);
+		VirtualAlloc(begin, sizeof(T) * maxElCount, MEM_COMMIT, PAGE_NOACCESS);
+	}
+
+#else
+
+	if (begin)
+	{
+		VirtualFree(begin, maxElCount * sizeof(T), MEM_RELEASE);
+	}
+
+	size = 0;
+
+#endif // PINNED_VECTOR_MEMORY_CHECK
+
+}
+
+template<class T, unsigned int maxElCount>
+inline PinnedVector<T, maxElCount>::~PinnedVector()
+{
+	free();
 }
--- pinnedVector/main.cpp
+++ pinnedVector/main.cpp
@@ -3,17 +3,34 @@
 
 int main()
 {
-	PinneVector<int> v;
 
-	v.resize(10);
+	int *ptr;
 
-	v[0] = 1;
 
-	std::cout << v[0] << "\n";
+	{
+		PinnedVector<int> v;
+		PinnedVector<int> v2;
 
-	v.resize(1200);
+		v.resize(10);
+		
 
-	std::cout << v[0] << "\n";
+		v[0] = 1;
+
+		ptr = &v[0];
+
+		std::cout << v[0] << "\n";
+
+		v.resize(1200);
+
+		PinnedVector<int> v3 = std::move(v);
+
+		std::cout << v3[0] << "\n";
+		
+	}
+
+	//std::cout << *ptr << "\n";
+
+	std::cout << "test";
 
 	return 0;
 }
